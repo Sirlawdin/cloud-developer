5c4f9f189392d29c3f2c985c33dd6b0e5f2907b2
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-07-17 12:46:54 +0100

    Udagram project

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,6 +13,8 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
+
+
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -28,14 +30,56 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
+  
+  app.get("/filteredimage", async (req, res) => {
+    const { image_url }: { image_url: string } = req.query;
+    
+    if (!image_url) {
+      res.status(400).send({ message: "image_url is required or malformed" });
+      return;
+    }
+
+    //Regular expression to validate the image_url
+    const expression: RegExp =
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+    const regex: RegExp = new RegExp(expression);
+
+    // validate if the image_url is well formed
+    if (!image_url.match(regex)) {
+      res.status(400).send({ message: "Image URL is required or malformed" });
+      return;
+    }
+
+    // validate the type of the image
+    if (
+      !image_url.toLowerCase().endsWith(".jpeg") &&
+      !image_url.toLowerCase().endsWith(".jpg") &&
+      !image_url.toLowerCase().endsWith(".png") &&
+      !image_url.toLowerCase().endsWith(".bmp") &&
+      !image_url.toLowerCase().endsWith(".tiff")
+    ) {
+      res.status(400).send({ message: "image not supported" });
+      return;
+    }
+
+    const promiseImage: Promise<string> = filterImageFromURL(image_url);
+
+    promiseImage
+      .then((image) => {
+        res.sendFile(image, () => {
+          const imagesToBeDeleted: Array<string> = new Array(image);
+          deleteLocalFiles(imagesToBeDeleted);
+        });
+      })
+      .catch((error) => {
+        res.status(404).send({ message: "image not found" });
+        return;
+      });
+  });
+  
   
 
   // Start the Server
